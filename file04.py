631393747d3e842f874598bd63852a4dffd78459
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-05-04 16:49:43 +0500

    4

--- file04.py
+++ file04.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    a=open(data,mode='r')
+    b=a.read()
+    z= ''
+    x=0
+    while x<len(b):
+        if b[x].isalpha():
+            z=z+b[x]
+        x+=1
+        
+    return list(z)
+print(main('txt_file/data04.txt'))
 # Read data from file
